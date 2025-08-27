cb8f112e3b5069879ce84558501660bf9778fe5f
Beka Addisu <bekafite@gmail.com>
2025-08-27 07:13:26 -0700

    Add tasks url.py and task

--- task_management_api/settings.py
+++ task_management_api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_management_api',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'tasks',
 ]
 
@@ -124,3 +125,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'tasks.User'
+# DRF global settings (optional but recommended)
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
---
+++ tasks/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import RegisterView, TaskViewSet, CategoryViewSet, CommentListCreateView
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename='task')
+router.register(r'categories', CategoryViewSet, basename='category')
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # User login & token retrieval
+    path('', include(router.urls)),
+    path('comments/<int:task_id>/', CommentListCreateView.as_view(), name='comment-list-create'),
+]
